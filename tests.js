b8aa7465133e0b9f67486ed9b8836361fb442fe5
fachal matias ezequiel <mfachal@ws3.labo4>
2018-02-26 20:20:10 -0300

    tests

--- tests.js
+++ tests.js
@@ -1,9 +1,10 @@
 // Test01
 bool = new Bool();
 flec_bool_bool = new flec(bool, bool);
-flec_bool_bool.toString() == "Bool -> Bool";
+flec_bool_bool.toString() == "Bool -> Boolo";
 
 
 absDeBool = new abs(new vars("x"),flec_bool_bool, new TT);
 
 absDeBool.toString() == "(\\x.Bool -> Bool: TT)";
+
